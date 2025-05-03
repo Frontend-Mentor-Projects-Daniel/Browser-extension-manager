56af6f6ac7aec875438125ebb7209cc7fb049937
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2025-05-03 00:56:48 -0400

    get html files and partials working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import '../assets/styles/main.css';
+import homepage from './adapters/html/homepage.html?raw';
+import header from './adapters/html/partials/header.html?raw';
+import footer from './adapters/html/partials/footer.html?raw';
+
+document.getElementById('app').innerHTML = `
+${header}
+${homepage}
+${footer}
+`;
