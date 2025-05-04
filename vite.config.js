b5c5f5392e81f582f9eae1289ee9a2786875dcd5
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2025-05-03 21:28:48 -0400

    set up header component, make responsive and add hover/focus styles

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.', // Set to current directory (default)
+  root: '.',
   build: {
-    outDir: 'dist', // Output directory for production build
-    emptyOutDir: true, // Clear outDir before build
-    assetsDir: 'assets', // Folder inside dist/ for CSS/fonts/images
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
   },
   server: {
-    open: true, // Auto-open in browser
-    port: 3000, // Change if needed
+    open: true,
+    port: 3000,
   },
 });
