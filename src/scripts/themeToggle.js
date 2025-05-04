164812e780c8b7a56d4c2fbc0eb7ac2a9297a180
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2025-05-04 00:23:25 -0400

    get icon to change on theme change

--- src/scripts/themeToggle.js
+++ src/scripts/themeToggle.js
@@ -2,22 +2,60 @@
 import granimInstance from './granim';
 
 const html = document.querySelector('html');
-const themeToggleButton = document.querySelector('.header__theme-toggle');
+const themeToggleButton = document.querySelectorAll('.header__theme-toggle');
 
-if (themeToggleButton instanceof HTMLButtonElement) {
-  themeToggleButton.addEventListener('click', () => {
-    // On button click, check the current value of the data-theme attribute
-    if (html instanceof HTMLHtmlElement) {
-      const currentTheme = html.getAttribute('data-theme');
-      // If data-theme="dark" set data-theme="light" and vice-versa
-      if (currentTheme === 'dark') {
-        html.setAttribute('data-theme', 'light');
-        // granim is for animating gradients
-        granimInstance.changeState('animated');
-      } else if (currentTheme === 'light') {
-        html.setAttribute('data-theme', 'dark');
-        granimInstance.changeState('static');
+themeToggleButton.forEach((btn) => {
+  if (btn instanceof HTMLButtonElement) {
+    btn.addEventListener('click', () => {
+      // On button click, check the current value of the data-theme attribute
+      if (html instanceof HTMLHtmlElement) {
+        const currentTheme = html.getAttribute('data-theme');
+        // If data-theme="dark" set data-theme="light" and vice-versa
+        if (currentTheme === 'dark') {
+          html.setAttribute('data-theme', 'light');
+          // granim is for animating gradients
+          granimInstance.changeState('animated');
+          renderIcon('sun');
+        }
+
+        if (currentTheme === 'light') {
+          html.setAttribute('data-theme', 'dark');
+          granimInstance.changeState('static');
+          renderIcon('moon');
+        }
+      }
+    });
+  }
+});
+
+/**
+ * Render new button with icon
+ * @param {"sun" | "moon"} dataSymbol
+ */
+function renderIcon(dataSymbol) {
+  if (dataSymbol === 'sun') {
+    const sunButton = document.querySelector('[data-symbol="sun"]');
+    const moonButton = document.querySelector('[data-symbol="moon"]');
+
+    if (sunButton instanceof HTMLButtonElement) {
+      sunButton.classList.add('hidden');
+
+      if (moonButton instanceof HTMLButtonElement) {
+        moonButton.classList.remove('hidden');
       }
     }
-  });
+  }
+
+  if (dataSymbol === 'moon') {
+    const sunButton = document.querySelector('[data-symbol="sun"]');
+    const moonButton = document.querySelector('[data-symbol="moon"]');
+
+    if (moonButton instanceof HTMLButtonElement) {
+      moonButton.classList.add('hidden');
+
+      if (sunButton instanceof HTMLButtonElement) {
+        sunButton.classList.remove('hidden');
+      }
+    }
+  }
 }
