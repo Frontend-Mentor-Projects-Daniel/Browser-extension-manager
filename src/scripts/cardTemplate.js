1b5b8efb81db1720fcf1fbda3250e13307158b87
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2025-05-19 12:52:30 -0400

    style pillswitch

--- src/scripts/cardTemplate.js
+++ src/scripts/cardTemplate.js
@@ -12,6 +12,20 @@ function createCard({ title, text, logoSrc, logoAlt }) {
   return clone;
 }
 
+function togglePillSwitch(pillSwitch) {
+  if (pillSwitch instanceof HTMLButtonElement) {
+    pillSwitch.addEventListener('click', () => {
+      const isOn = pillSwitch.getAttribute('aria-pressed') === 'true';
+      pillSwitch.setAttribute('aria-pressed', String(!isOn));
+
+      const ariaText = document.querySelector('.card__status-indicator-text');
+      if (ariaText instanceof HTMLSpanElement) {
+        ariaText.textContent = isOn ? 'Extension InActive' : 'Extension Active';
+      }
+    });
+  }
+}
+
 async function loadCards() {
   const res = await fetch('assets/data/data.json');
   if (!res.ok) {
@@ -27,7 +41,13 @@ async function loadCards() {
       logoSrc: logo,
       logoAlt: name,
     });
+
     list.appendChild(card);
+  });
+
+  const pillSwitch = document.querySelectorAll('.card__switch');
+  pillSwitch.forEach((sw) => {
+    togglePillSwitch(sw);
   });
 }
 
